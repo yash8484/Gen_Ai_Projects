51f8babbe1265c3896109cd5a5642fb66533b215
Yash Sontakke <75352487+yash8484@users.noreply.github.com>
2025-07-26 22:48:35 +0530

    adding requirements.txt file

---
+++ Complete_Medical_Chatbot_Assistant_with_LLM_Langchain_Pinecode_RAG_Flask_AWS/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot_assistant",
+    version="0.1.0",
+    author="Yash Sontakke",
+    author_email="yashsontakke8484@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
